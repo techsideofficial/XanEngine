f45c96f021f21cb4cb9102c6f9f87ea866a27b2a
techsideofficial <wienerwolf77@gmail.com>
2023-08-17 09:59:29 +1200

    e

--- addons/FMOD/editor/fmod_guids.gd
+++ addons/FMOD/editor/fmod_guids.gd
@@ -7,14 +7,27 @@
 class_name FMODGuids
 
 class Events:
-	const POST = "{a8a2b59d-b143-4f9c-9a85-e2c5470d30c6}"
-	const PRE = "{5a547e29-2810-43f6-b837-5683b62a9dfc}"
+	const BATTLEPASS_POST = "{a8a2b59d-b143-4f9c-9a85-e2c5470d30c6}"
+	const BATTLEPASS_PRE = "{5a547e29-2810-43f6-b837-5683b62a9dfc}"
+	const EMOTES_LLAMABELL_LLAMABELL_INTRO = "{d1679176-b2aa-420a-99c0-d697bc0c398d}"
+	const EMOTES_LLAMABELL_LLAMABELL_LOOP = "{9e413c51-b25d-4e73-bfd5-56d0cb981b03}"
+	const EMOTES_LLAMABELL_LLAMABELL_MAIN = "{90d2c3d7-cfe4-4ad6-b349-1f93d8b8f1dd}"
+	const EMOTES_ZIRCONSWEEP = "{5397b4f8-00bc-4bb4-94a9-a25f8c82fbb0}"
+	const MUSICPACKS_DEFAULT_REMIX = "{6c7e233e-1a69-40e1-afc4-3fc5d651b822}"
+	const MUSICPACKS_EVERYBODYDANCE = "{b5ac4cd7-78ab-4b2e-98c0-5fb5ff1e7129}"
+	const MUSICPACKS_FLOSS = "{499acb3a-b27a-419b-9e44-7146570c7d90}"
+	const MUSICPACKS_FUTURE_REMIX = "{2a8d9367-c969-4df0-b189-086525f6d202}"
+	const MUSICPACKS_HOMEBASE = "{8c9e359a-8786-4267-a1eb-43179e041682}"
+	const MUSICPACKS_LILWHIP = "{358ab2fe-d86f-4192-a2a2-ca5ae45718b4}"
+	const MUSICPACKS_LLAMABELL = "{ba49b326-d500-4197-a50b-6367be2dd047}"
 
 class Busses:
 	const MASTER_BUS = "{b6f84801-e3df-441a-a2c7-d79685596dbc}"
-	const REVERB = "{5fae1b69-4342-4373-abe2-dece324d432c}"
 
 class Banks:
+	const EMOTES = "{46eedf6f-d3b4-4ff9-8dae-04e2e693a146}"
 	const MASTER = "{225be7eb-048e-4e61-997f-b16601010e2e}"
-	const MUSIC = "{916aa71e-271a-4665-b9b0-a34867cf14be}"
+	const MUSICPACKS = "{7eb7cd4e-5869-48ae-8b21-3a3ac0ea25a0}"
+	const SFX = "{9815987a-7dbf-4b95-8120-9e6b10991f19}"
+	const UI = "{74234b8e-d6f9-48af-b9d0-cbf4070792b4}"
 
