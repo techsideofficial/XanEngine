aef046d6de829c8035f2a14702a9f252b2ad61bc
techsideofficial <wienerwolf77@gmail.com>
2023-08-17 09:54:01 +1200

    e

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,20 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'bpps_music.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const POST = "{a8a2b59d-b143-4f9c-9a85-e2c5470d30c6}"
+	const PRE = "{5a547e29-2810-43f6-b837-5683b62a9dfc}"
+
+class Busses:
+	const MASTER_BUS = "{b6f84801-e3df-441a-a2c7-d79685596dbc}"
+	const REVERB = "{5fae1b69-4342-4373-abe2-dece324d432c}"
+
+class Banks:
+	const MASTER = "{225be7eb-048e-4e61-997f-b16601010e2e}"
+	const MUSIC = "{916aa71e-271a-4665-b9b0-a34867cf14be}"
+
