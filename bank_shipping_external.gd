0f04a9d760cf444bb7510bae78c227dbe06ca2bb
techsideofficial <wienerwolf77@gmail.com>
2023-08-17 14:12:53 +1200

    e

--- bank_shipping_external.gd
+++ bank_shipping_external.gd
@@ -1,10 +1,10 @@
 extends Node
 
-var externalbanks = OS.get_executable_path().get_base_dir() + "/CookedPCConsole"
+var mp_default = "event:/MusicPacks/og_classic"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	RuntimeManager.play_one_shot_path(mp_default)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
