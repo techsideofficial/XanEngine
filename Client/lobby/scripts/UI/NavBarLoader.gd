9f555c71b85c83e7170c35e88db5b0ccdb328682
techsideofficial <wienerwolf77@gmail.com>
2023-08-21 08:40:22 +1200

    e

---
+++ Client/lobby/scripts/UI/NavBarLoader.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name NavBar
+
+var PlayTab = load("res://lobby/scenes/PlayTab.tscn").instance()
+var LockerTab = load("res://lobby/scenes/LockerTab.tscn")
+
+func PlayTab
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
