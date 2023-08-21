a865fe5eb602182d7f9508c541ded190641ff3e3
techsideofficial <wienerwolf77@gmail.com>
2023-08-21 13:05:48 +1200

    i want to die

---
+++ Client/lobby/scripts/locker.gd
@@ -0,0 +1,26 @@
+extends Node
+class_name locker #why the fuck do I have to to this :(
+
+func playPack(packName):
+	RuntimeManager.play_one_shot_path("event:/MusicPacks/" + packName)
+
+func select
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
