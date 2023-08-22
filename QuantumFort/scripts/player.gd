c966bb839e46d1ed3619d71543eb29a6e8d591f9
techsideofficial <wienerwolf77@gmail.com>
2023-08-23 09:16:06 +1200

    e

--- QuantumFort/scripts/player.gd
+++ QuantumFort/scripts/player.gd
@@ -2,8 +2,8 @@ extends Node
 class_name qplayer
 
 func get_songs():
-	var bPak = FMODStudioModule.get_studio_system().get_bank("bank:/MusicPacks")
-	print(bPak.get_event_list(11))
+	var bPak = FMODStudioModule.get_studio_system().get_bank("res://fmod/Build/Desktop/MusicPacks.bank")
+	print(bPak.get_event_list(2))
 
 func _ready():
 	get_songs()
