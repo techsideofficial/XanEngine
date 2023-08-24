1c469efdaf7bdcd9c7b92547e0ef552f77d72411
techsideofficial <wienerwolf77@gmail.com>
2023-08-25 08:39:26 +1200

    e

--- XanEngine/addons/XanEngine/XanEngine.gd
+++ XanEngine/addons/XanEngine/XanEngine.gd
@@ -4,7 +4,13 @@ class_name XanEngine
 func SceneChange(path: String):
 	get_tree().change_scene_to_file(path)
 	
-
+func ConfigExists(path):
+	var config = ConfigFile.new()
+	var err = config.load(path)
+	if err == OK:
+		return true
+	else:
+		return false
 
 func _ready():
 	pass
