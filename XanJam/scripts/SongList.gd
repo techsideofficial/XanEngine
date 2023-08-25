e9be770897a181169fce9b38918a16a0a93c77af
techsideofficial <wienerwolf77@gmail.com>
2023-08-25 14:14:37 +1200

    e

--- XanJam/scripts/SongList.gd
+++ XanJam/scripts/SongList.gd
@@ -3,9 +3,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var XanEngine
 	#XanEngine.new().ConfigSave("TestData", "TestVersion", 1.0, "XanDefaultEngine")
 	var events = XanEngine.new().ConfigLoad("BankEventCache", "xcache", "BankCache", "DownloadedEvents")
+	RuntimeManager.play_one_shot_path("event:/MusicPacks/" + events[2])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
