059f7959193e8484e65fd1feec456cd1b16d4787
techsideofficial <wienerwolf77@gmail.com>
2023-08-25 15:18:35 +1200

    e

--- XanJam/scripts/SongList.gd
+++ XanJam/scripts/SongList.gd
@@ -5,9 +5,11 @@ extends Node
 func _ready():
 	#XanEngine.new().ConfigSave("TestData", "TestVersion", 1.0, "XanDefaultEngine")
 	var events = XanEngine.new().ConfigLoad("BankEventCache", "xcache", "BankCache", "DownloadedEvents")
+	
+	ProjectSettings.load_resource_pack("user://XanEngine/Banks/banks.pck")
 	print(events)
-	print("event:/MusicPacks/" + events[1])
-	#RuntimeManager.play_one_shot_path(bankFile + events[1])
+	print(events[1])
+	RuntimeManager.play_one_shot_path("res://MusicPacks.bank/events/" + events[1])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
