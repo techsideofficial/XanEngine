58edf29ec0699698297bf300016dab3618108264
techsideofficial <wienerwolf77@gmail.com>
2023-08-25 17:10:49 +1200

    e

--- XanJam/scripts/SongList.gd
+++ XanJam/scripts/SongList.gd
@@ -5,12 +5,13 @@ extends Node
 func _ready():
 	#XanEngine.new().ConfigSave("TestData", "TestVersion", 1.0, "XanDefaultEngine")
 	var events = XanEngine.new().ConfigLoad("BankEventCache", "xcache", "BankCache", "DownloadedEvents")
-	
+	FMODStudioModule.get_studio_system().unload_all()
 	ProjectSettings.load_resource_pack("user://XanEngine/Banks/banks.pck")
-	var loaded = FMODStudioModule.get_studio_system().load_bank_file("res://MusicPacks.bank", 0, false)
+	FMODStudioModule.get_studio_system().load_bank_file("res://Master.bank", 0, false)
+	FMODStudioModule.get_studio_system().load_bank_file("res://Master.strings.bank", 0, false)
+	FMODStudioModule.get_studio_system().load_bank_file("res://MusicPacks.bank", 0, false)
 	print(events[1])
-	print(loaded)
-	RuntimeManager.play_one_shot_path("event:/MusicPacks/everybodydance")
+	RuntimeManager.play_one_shot_path("event:/MusicPacks/placeholder")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
