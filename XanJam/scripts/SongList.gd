b99ed8ccd610eccbddd722e1ffd2f4470fb50a7d
techsideofficial <wienerwolf77@gmail.com>
2023-08-27 11:45:16 +1200

    e

--- XanJam/scripts/SongList.gd
+++ XanJam/scripts/SongList.gd
@@ -7,9 +7,10 @@ func _ready():
 	var songs = get_node("/root/SongBrowser/ItemList")
 	get_node("/root/SongBrowser/ItemList").item_activated.connect(itemClicked)
 	#XanEngine.new().ConfigSave("TestData", "TestVersion", 1.0, "XanDefaultEngine")
-	var events = XanEngine.new().ConfigLoad("BankEventCache", "xcache", "BankCache", "DownloadedEvents", "Cache")
 	XanEngine.new().reloadBanks("user://XanEngine/Paks/DefaultBanks.xax")
+	var events = XanEngine.new().ConfigLoad("res://XanMod/contentref.txt", "BankCache", "LoadedContent")
 	#RuntimeManager.play_one_shot_path("event:/MusicPacks/" + events[3])
+	print(events)
 	
 	for item in events:	
 		get_node("/root/SongBrowser/ItemList").add_item(item, null, true)
@@ -22,5 +23,5 @@ func itemClicked(index):
 	FMODStudioModule.get_studio_system().get_bus("bus:/").stop_all_events(FMODStudioModule.FMOD_STUDIO_STOP_IMMEDIATE)
 	var songs = get_node("/root/SongBrowser/ItemList")
 	var songName = songs.get_item_text(index)
-	var currentSong = RuntimeManager.create_instance_path("event:/MusicPacks/" + songName)
+	var currentSong = RuntimeManager.create_instance_path("event:/" + songName)
 	currentSong.start()
