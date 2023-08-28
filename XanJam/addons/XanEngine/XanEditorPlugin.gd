f467678e6eaea775cd7e454da8b844b257257aba
techsideofficial <wienerwolf77@gmail.com>
2023-08-29 09:59:30 +1200

    e

--- XanJam/addons/XanEngine/XanEditorPlugin.gd
+++ XanJam/addons/XanEngine/XanEditorPlugin.gd
@@ -2,14 +2,39 @@
 extends EditorPlugin
 
 
+const MainPanel: PackedScene = preload("res://addons/XanEngine/studio/main_panel.tscn")
+
+var main_panel_instance
+
+
 func _enter_tree():
 	add_custom_type("XanEngine", "Node", preload("scripts/XanEngine.gd"), preload("assets/ico_xan16.png"))
 	DirAccess.make_dir_absolute("user://XanEngine/")
 	DirAccess.make_dir_absolute("user://XanEngine/Config")
 	DirAccess.make_dir_absolute("user://XanEngine/Banks")
-	XanEngine.new().log("Initialised XanEngine")
+	main_panel_instance = MainPanel.instantiate()
+	get_editor_interface().get_editor_main_screen().add_child(main_panel_instance)
+	_make_visible(false)
 
 
 func _exit_tree():
-	XanEngine.new().log("Shutting Down XanEngine")
+	if main_panel_instance:
+		main_panel_instance.queue_free()
 	remove_custom_type("XanEngine")
+
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+
+func _get_plugin_name():
+	return "XanEngine"
+
+
+func _get_plugin_icon():
+	return load("res://addons/XanEngine/assets/ico_xan16.png")
---
+++ XanJam/addons/XanEngine/studio/dir_structure.gd
@@ -0,0 +1,13 @@
+@tool
+extends CenterContainer
+
+func _on_dirstruct_pressed():
+	DirAccess.make_dir_absolute("res://XanMod/")
+	DirAccess.make_dir_absolute("res://XanMod/content/")
+	var conf = ConfigFile.new()
+	var conf2 = ConfigFile.new()
+	conf.set_value("BankCache", "LoadedContent", ["SONG_1", "SONG_2", "..."])
+	conf2.set_value("INFO", "INFO", " This folder should contain the files 'Master.bank, Master.strings.bank, and Xan.bank'")
+	conf.save("res://XanMod/contentref.txt")
+	conf2.save("res://XanMod/content/README.txt")
+	
