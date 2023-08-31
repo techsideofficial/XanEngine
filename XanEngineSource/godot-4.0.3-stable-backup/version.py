b30a5d2025206b4ec710a39fed7e655e53450347
techsideofficial <wienerwolf77@gmail.com>
2023-08-31 18:00:53 +1200

    e

---
+++ XanEngineSource/godot-4.0.3-stable-backup/version.py
@@ -0,0 +1,10 @@
+short_name = "xan"
+name = "Xan Engine"
+major = 3
+minor = 0
+patch = 0
+status = "stable"
+module_config = ""
+year = 2023
+website = "https://github.com/techsideofficial/XanEngine"
+docs = "4.0"
